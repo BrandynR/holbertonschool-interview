c93676371f91ffe02507a5effdae88171aedd0d9
BrandynR <brandynr@github.com>
2020-05-12 10:53:18 -0400

    Made executable, fixed Pep8

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""fewest operations needed"""
 
 
 def minOperations(n):
@@ -9,14 +10,24 @@ def minOperations(n):
     Returns:
         fewest needed number of  operations
     """
-    sum = 0
-    i = 2
-    while i * i <= n:
-        if n % i:
-            i += 1
+
+    Actual = 1
+    num_of_ops = 0
+
+    if type(n) is not int or n < 2:
+        return 0
+
+    while (Actual != n):
+
+        if (n % Actual == 0):
+            CopyAll = Actual
+            Paste = Actual + CopyAll
+            num_of_ops += 2
+
         else:
-            n //= i
-            sum += i
-    if n > 1:
-        sum += n
-    return sum
+            Paste = Actual + CopyAll
+            num_of_ops += 1
+
+        Actual = Paste
+
+    return num_of_ops
