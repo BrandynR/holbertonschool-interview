92099188d5e382fa1e68f95a9fe146b2512ba09b
BrandynR <brandynr@github.com>
2020-05-12 10:46:31 -0400

    Made executable, fixed Pep8

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
+
 def minOperations(n):
-    """calculates the fewest number of operations needed to result in exactly n H characters in the file
+    """calculates the fewest number of operations needed to result
+    in exactly n H characters in the file.
     Args:
         n: characters to reach
     Returns:
