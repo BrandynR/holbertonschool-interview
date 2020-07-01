dcc7909b650cac3c39bdbda6ce07c9158982cd36
BrandynR <brandynr@github.com>
2020-07-01 19:48:52 -0400

    typo fix

--- 0x0A-slide_line/0-slide_line.c
+++ 0x0A-slide_line/0-slide_line.c
@@ -8,107 +8,37 @@
  * @direction: left or right
  * Return: 1 on success 0 on failure
  */
+
 int slide_line(int *line, size_t size, int direction)
 {
+	int s = size, n_line[99];
+	int i = direction == SLIDE_LEFT ? 0 : size - 1, n = i, x = 0;
 
-	if (direction == 0)
-		slide_left(line, size);
-	else if (direction == 1)
-		slide_right(line, size);
-	else
+	if (direction != SLIDE_LEFT && direction != SLIDE_RIGHT)
 		return (0);
+	while (x < 99)
+	{
+		n_line[x] = 0;
+		x++;
+	}
+	while (direction == SLIDE_LEFT ? i < s : i >= 0)
+	{
+		if (line[i] != 0)
+		{
+			if (n_line[n] && line[i] == n_line[n])
+			{
+				n_line[n] += line[i];
+				direction == SLIDE_LEFT ? n++ : n--;
+			} else if (n_line[n] && line[i] != n_line[n])
+			{
+				direction == SLIDE_LEFT ? n++ : n--;
+				n_line[n] += line[i];
+			} else if (!n_line[n])
+				n_line[n] += line[i];
+		}
+		direction == SLIDE_LEFT ? i++ : i--;
+	}
+	for (i = 0; i < s; i++)
+		line[i] = n_line[i];
 	return (1);
-}
-
-/**
- * slide_right - slides all numbers to the right
- * @line: array of integers
- * @size: size of array
- */
-void slide_right(int *line, size_t size)
-{
-	int i, next, j;
-
-	for (i = (int) size - 1, next = i - 1; next >= 0;)
-	{
-		if (!line[i])
-		{
-			i--;
-			next--;
-			continue;
-		}
-		while (next >= 0 && line[next] == 0)
-			next--;
-		if (next < 0)
-			break;
-		if (line[i] == line[next])
-		{
-			line[i] *= 2;
-			line[next] = 0;
-			i = next - 1;
-			next = i - 1;
-		}
-		else
-		{
-			i = next;
-			next--;
-		}
-	}
-	i = (int)size - 1;
-	for (j = (int)size - 1; j >= 0; j--)
-	{
-		if (line[j] != 0)
-		{
-			line[i] = line[j];
-			if (j < i)
-				line[j] = 0;
-			i--;
-		}
-	}
-}
-/**
- * slide_left - slides all numbers to the right
- * @line: array of integers
- * @size: size of array
- */
-void slide_left(int *line, size_t size)
-{
-	int i, next, j;
-
-	for (i = 0, next = 1; next < (int) size;)
-	{
-		if (!line[i])
-		{
-			i++;
-			next++;
-			continue;
-		}
-		while (next < (int) size && line[next] == 0)
-			next++;
-		if (next >= (int) size)
-			break;
-		if (line[i] == line[next])
-		{
-			line[i] *= 2;
-			line[next] = 0;
-			i = next + 1;
-			next = i + 1;
-		}
-		else
-		{
-			i = next;
-			next = i + 1;
-		}
-	}
-	i = 0;
-	for (j = 0; j < (int)size; j++)
-	{
-		if (line[j] != 0)
-		{
-			line[i] = line[j];
-			if (j > i)
-				line[j] = 0;
-			i++;
-		}
-	}
 }
