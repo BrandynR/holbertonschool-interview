cdb98cf4702140e4d25a9c95b2f724cdaa6e5ce1
BrandynR <brandynr@github.com>
2020-11-16 14:25:23 -0500

    Add a new node to the end/beginning of a double circular linked list

---
+++ 0x1A-double_circular_linked_list/0-add_node.c
@@ -0,0 +1,72 @@
+#include "list.h"
+
+/**
+ * new_node - creates a node in linked list
+ * @str: string for list of data
+ * Return: newly created node
+ */
+List *new_node(char *str)
+{
+	List *new;
+
+	new = malloc(sizeof(*new));
+	if (!new)
+		return (NULL);
+	new->str = strdup(str);
+	if (!new->str)
+		return (NULL);
+	return (new);
+}
+
+/**
+ * new_pointer - pointers are updated
+ * @list: linked list
+ * @new: pointer to new node
+ */
+void new_pointer(List **list, List *new)
+{
+	if (!*list)
+	{
+		*list = new, (*list)->next = (*list)->prev = new;
+	}
+	else
+	{
+		new->prev = (*list)->prev, new->next = *list;
+		(*list)->prev = (*list)->prev->next = new;
+	}
+}
+
+/**
+ * add_node_end - Add a new node to the end of a double circular linked list
+ * @list: the list to modify
+ * @str: the string to copy into the new node
+ * Return: Address of the new node, or NULL on failure
+ */
+List *add_node_end(List **list, char *str)
+{
+	List *new;
+
+	new = new_node(str);
+	if (!new)
+		return (NULL);
+	new_pointer(list, new);
+	return (new);
+}
+
+/**
+ * add_node_begin - Add a new node to the beginning of a double circular linked list
+ * @list: the list to modify
+ * @str: the string to copy into the new node
+ * Return: Address of the new node, or NULL on failure
+ */
+List *add_node_begin(List **list, char *str)
+{
+	List *new;
+
+	new = new_node(str);
+	if (!new)
+		return (NULL);
+	new_pointer(list, new);
+	*list = new;
+	return (new);
+}
