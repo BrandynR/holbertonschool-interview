5c844e45815251d8d33292f2f64218768cb94cda
BrandynR <brandynr@github.com>
2021-01-13 17:49:20 -0500

    function that checks whether a given pattern matches a given string.

---
+++ 0x20-regex/regex.c
@@ -0,0 +1,32 @@
+#include "regex.h"
+
+/**
+* regex_match - function that checks whether a given pattern matches a given string.
+* @str: is the string to scan
+* @pattern: is the regular expression
+* Return: return 1 if true, 0 if not
+*/
+int regex_match(char const *str, char const *pattern)
+{
+	int none = 0;
+
+	if (str == NULL)
+		return (none);
+	if (*str == NULL)
+	{
+		if (*(pattern + 1) == '*')
+			return (regex_match(str, pattern + 2));
+		return (!*pattern);
+	}
+	if (*(pattern + 1) == '*')
+	{
+		if (*pattern == '.' || *pattern == *str)
+			return (regex_match(str + 1, pattern) || regex_match(str, pattern + 2));
+		return (regex_match(str, pattern + 2));
+	}
+	if (*pattern == '.' || *pattern == *str)
+	{
+		return (regex_match(str + 1, pattern + 1));
+	}
+	return (none);
+}
---
+++ 0x20-regex/regex.h
@@ -0,0 +1,6 @@
+#ifndef _REGEX_HEAD_
+#define _REGEX_HEAD_
+
+int regex_match(char const *str, char const *pattern);
+
+#endif /*_REGEX_HEAD_*/
